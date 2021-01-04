cba4f3839789e843a3329337bae64823668de658
GuramandeepGill <guramandeepg@gmail.com>
2021-01-04 15:54:32 -0800

    Add displayStudents, buildStudentProfile, and calculateAverage functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
+const calculateAverage = grades => {
+   let total = 0;
+   grades.forEach(grade => {
+      total += parseInt(grade);
+   });
+   return `${total/grades.length}`;
+}
+
+const buildStudentProfile = student => {
+   let profileDiv = document.createElement('div');
+   profileDiv.innerHTML = `
+      <img src='${student.pic}' alt='${student.firstName} ${student.lastName}'>
+      <h1>${student.firstName} ${student.lastName}</h1>
+      <p>Email: ${student.email}</p>
+      <p>Company: ${student.company}</p>
+      <p>Skill: ${student.skill}</p>
+      <p>Average: ${calculateAverage(student.grades)}</p>
+   `;
+   return profileDiv;
+}
+
+const displaystudents = students => {
+   let contentDiv = document.getElementById('content');
+   students.forEach(student => {
+      contentDiv.append(buildStudentProfile(student));
+   });
+}
+
 const main = () => {
    let url = 'https://api.hatchways.io/assessment/students';
 
@@ -5,6 +33,7 @@ const main = () => {
       .then(response => response.json())
       .then(data => {
          let {students} = data;
+         displaystudents(students);
          console.log(students);
       });
    // console.log('hi');
