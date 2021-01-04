91fc0dc91730b418b025808c2ead88760cdde16b
GuramandeepGill <guramandeepg@gmail.com>
2021-01-04 15:36:47 -0800

    Fetch data from the api and display it on console window

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 const main = () => {
-   console.log("Hi");
+   let url = 'https://api.hatchways.io/assessment/students';
+
+   fetch(url)
+      .then(response => response.json())
+      .then(data => {
+         let {students} = data;
+         console.log(students);
+      });
+   // console.log('hi');
 }
 window.onload = main;
